12231bb4ce71685de444eaba958ab5c83b6bf04f
ebenezer.botelho <ebenezer.botelho@trt10.jus.br>
2018-12-12 12:37:00 -0200

    Animação do cardápio de restaurantes

--- src/app/restaurant-detail/menu-item/menu-item.component.ts
+++ src/app/restaurant-detail/menu-item/menu-item.component.ts
@@ -1,13 +1,26 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { MenuItem } from './menu-item.model';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'mt-menu-item',
-  templateUrl: './menu-item.component.html'
+  templateUrl: './menu-item.component.html',
+  animations: [
+    trigger('menuItemAppeared', [
+      state('ready', style({opacity: 1})),
+      transition('void => ready', [
+        style({opacity: 0, transform: 'translateY(-20px)'}),
+        animate('300ms 0s ease-in')
+      ])
+    ])
+  ]
 })
 export class MenuItemComponent implements OnInit {
 
+  menuItemState: string = 'ready';
+
   @Input() menuItem: MenuItem;
+
   @Output() add = new EventEmitter(); //Criando um evento para o meu componente (tipo um onClick, onKeyUp etc.).
                                       //O nome do evento é o nome desta propriedade, isto é, "add".
                                       //No componente, eu devo dizer qual é o método que será chamado quando

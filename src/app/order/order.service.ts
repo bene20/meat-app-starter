aeff3d275e02e43fe19ed3effdcee4ca2d15f6bd
ebenezer.botelho <ebenezer.botelho@trt10.jus.br>
2018-12-13 18:35:58 -0200

    Migração da aplicação do angular 4.0 para o angular 4.3.3

--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -3,13 +3,15 @@ import { ShoppingCartService } from 'app/restaurant-detail/shopping-cart/shoppin
 import { CartItem } from 'app/restaurant-detail/shopping-cart/cart-item.model';
 import { Order } from './order.model';
 import { Observable } from 'rxjs/Observable';
-import { Http, RequestOptions, Headers } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
 import { MEAT_API } from 'app/app.api';
+
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class OrderService {
 
-  constructor(private cartService: ShoppingCartService, private http: Http) {}
+  constructor(private cartService: ShoppingCartService, private http: HttpClient) {}
 
   itemsValue(): number {
     return this.cartService.total();
@@ -32,13 +34,8 @@ export class OrderService {
   }
 
   checkOrder(order: Order): Observable<string> {
-    const headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-
-    return this.http.post(`${MEAT_API}/orders`, JSON.stringify(order), new RequestOptions({headers: headers}))
-                    .map(response => response.json())
-                    .map( (order) => order.id)
-                    ;
+    return this.http.post<Order>(`${MEAT_API}/orders`, order)
+                    .map( ord => ord.id);
   }
 
   clear() {

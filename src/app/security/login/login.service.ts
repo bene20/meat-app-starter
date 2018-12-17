1760a3bfe21d934fe6b1c32d5a52b3d97da58212
ebenezer.botelho <ebenezer.botelho@trt10.jus.br>
2018-12-17 15:14:35 -0200

    Implementado mecanismo de redirecionamento da tela de login para a página em execução anterior à ela de login

--- src/app/security/login/login.service.ts
+++ src/app/security/login/login.service.ts
@@ -5,15 +5,22 @@ import { MEAT_API } from 'app/app.api';
 import { User } from './user.model';
 
 import 'rxjs/add/operator/do';
-import { Router } from '@angular/router';
+import 'rxjs/add/operator/filter';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Injectable()
 export class LoginService {
 
   user: User;
+  lastUrl: string;
 
   constructor(private http: HttpClient,
-              private router: Router) {}
+              private router: Router) {
+    // Abaixo me inscrevo em todos os eventos do Router (todas as navegações) e salvo sempre a última rota acessada
+    this.router.events
+               .filter(e => e instanceof NavigationEnd)
+               .subscribe((e: NavigationEnd) => this.lastUrl = e.url);
+  }
 
   isLoggedIn(): boolean {
     return this.user !== undefined;
@@ -24,7 +31,11 @@ export class LoginService {
                     .do(user => this.user = user);
   }
 
-  handleLogin(path: string) {
+  handleLogin(path: string = this.lastUrl) {
     this.router.navigate(['/login', btoa(path)]); // btoa é uma função de encoding para base64
+  }
+
+  logout(): any {
+    this.user = undefined;
   }
 }
